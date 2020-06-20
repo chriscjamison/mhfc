195bce80307ab404ccf854ca01b7572e71e5c098
Chris Jamison <chris@chriscjamison.com>
2020-06-19 22:03:41 -0700

    0.10 - Main menu working

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,9 @@ $(document).ready(
         var $ellipse_kente_selector = "";
         // A String that will hold the CSS selector for the HTML element containing 
         // the border with a kente pattern is initialized.
+        var $menu_button_icon_selector = "";
+        // A String that will hold the CSS selector for the HTML element containing 
+        // the icon used for the menu button is initialized.
 
         var $menu_options_container_element = {};
         // An Object that will hold the jQuery object that refers to the list 
@@ -33,6 +36,9 @@ $(document).ready(
         var $ellipse_kente_element = {};
         // An Object that will hold the jQuery object that refers to the border 
         // with a kente pattern is initialized.
+        var $menu_button_icon_element = {};
+        // An Object that will hold the jQuery object that refers to the icon 
+        // used for the main menu button is initialized.
 
         $menu_options_container_selector = "#mobile-menu-options-div";
         // The CSS selector that refers to the HTML element referencing the 
@@ -40,6 +46,9 @@ $(document).ready(
         $ellipse_kente_selector = "#ellipse-kente";
         // The CSS selector that refers to the HTML element referencing the 
         // border with a kente pattern is passed on.
+        $menu_button_icon_selector = "#mobile-nav-icon-img";
+        // The CSS selector that refers to the HTML element referencing the 
+        // icon used for the main menu is passed on.
 
         $menu_options_container_element = $menu_options_container_selector;
         // The jQuery element that refers to the HTML element containing 
@@ -47,6 +56,9 @@ $(document).ready(
         $ellipse_kente_element = $ellipse_kente_selector;
         // The jQuery element that refers to the HTML element containing 
         // the border with a kente pattern is passed on.
+        $menu_button_icon_element = $menu_button_icon_selector;
+        // The jQuery element that refers tot he HTML element containing 
+        // the icon used for the main menu is passed on.
 
         var $menu_options_container_visible_selector = "";
         // A String that will hold the value of a CSS class that triggers the 
@@ -55,14 +67,38 @@ $(document).ready(
         var $ellipse_kente_shadow_selector = "";
         // A String that will hold the value of a CSS class that triggers 
         // a different drop shadow for the border with a kente pattern is initialized.
+        var $menu_button_icon_src_value = "";
+        // A String that will hold the value of a link to the 'close' icon used 
+        // for the main menu is initialized.
 
+        var $current_menu_button_icon_src_value = "";
+        // A String that will hold the current value of the attribute 'href' for 
+        // the icon used in the main menu is initialized.
        
         $menu_options_container_visible_selector = "menu-visible";
         $ellipse_kente_shadow_selector = "menu-ellipse-kente-shadow";
 
+        $current_menu_button_icon_src_value = $($menu_button_icon_element).attr("src");
+
+        var $current_menu_button_icon_index_value;
+        // A Number variable that will hold the index value for the location 
+        // of the string, 'menu.svg', is initialized.
+
+        $current_menu_button_icon_index_value = $current_menu_button_icon_src_value.indexOf("menu.svg");
+
+        // IF/ELSE statement that toggles the menu icon used in the main menu to 
+        // 'close' if the visitor is attempting to open the menu.
+        if ($current_menu_button_icon_index_value > -1) {
+          $menu_button_icon_src_value = "/assets/img/common/icons/close.svg";
+        } else {
+          $menu_button_icon_src_value = "/assets/img/common/icons/menu.svg";
+        }
         
-        $($menu_options_container_element).toggleClass($menu_options_container_visible_selector);
+        $($menu_options_container_element).slideToggle(400);
         $($ellipse_kente_element).toggleClass($ellipse_kente_shadow_selector);
+        $($menu_button_icon_element).attr("src", $menu_button_icon_src_value);
+
+
       }
     );
   }
